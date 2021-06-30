6726a5f87c664020fa0e3e9e394ee1ed16764201
anailson ribeiro <arsantos315@gmail.com>
2021-06-30 13:39:36 -0300

    Model Alimento

---
+++ src/app/shared/food.model.ts
@@ -0,0 +1,14 @@
+
+export class Food {
+
+        
+    constructor(public name: string, amount: number){
+            
+    }
+}
+
+
+/*MODEL PARA SERA UTILIZADO EM OUTROS MODEL OU SEJA COMPARTILHADO */
+/*Classe para ser compartilhadas com outras. Seguindo padrão*/
+/**public name: string;
+public amount : number; */
